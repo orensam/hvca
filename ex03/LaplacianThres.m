e05e2546d9f237e0d1a7e755a668fa43949adc7b
Oren Samuel <oren.samuel@mail.huji.ac.il>
2015-05-20 14:00:48 +0300

    helper for q4-6

---
+++ ex03/LaplacianThres.m
@@ -0,0 +1,16 @@
+function [] = LaplacianThres(I, thres)
+%% ex3 q4-6 helper
+    [Ix, Iy] = ImageDerivatives(I);
+    L = abs(Deriv2Laplace(Ix, Iy));
+    Lbin = L >= thres;
+
+     figure;
+     subplot(1, 2, 1);     
+     imshow(L);
+     title('Image Laplacian');
+     
+     subplot(1,2,2);     
+     imshow(Lbin);
+     title('Image Laplacian after thresholding');
+
+end
