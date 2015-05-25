03e3efffa8db17167d4a32a430d4d8039ec4475c
Oren Samuel <oren.samuel@mail.huji.ac.il>
2015-05-25 15:05:31 +0300

    q4-6 fixes

--- ex03/q4.m
+++ ex03/q4.m
@@ -1,7 +1,7 @@
 function [] = q4()
 %% ex3 q4
     I = double(imread('simul_cont_squares.tif'));
-    thres = 30;
+    thres = 10;
     LaplacianThres(I, thres);     
      
 end
--- ex03/q6.m
+++ ex03/q6.m
@@ -1,7 +1,31 @@
 function [] = q6()
 %% ex3 q6
     I = double(imread('kofka_ring.tif'));
-    thres = 16;
-    LaplacianThres(I, thres);     
+    thres = [10, 15, 20];
+    %LaplacianThres1(I, thres);     
+    
+    [Ix, Iy] = ImageDerivatives(I);
+    L = abs(Deriv2Laplace(Ix, Iy));
+    Lbin1 = L >= thres(1);
+    Lbin2 = L >= thres(2);
+    Lbin3 = L >= thres(3);
+
+     figure;
+     subplot(2, 2, 1);     
+     imshow(L);
+     title('Image Laplacian');
      
+     subplot(2,2,2);     
+     imshow(Lbin1);
+     title('Image Laplacian after thresholding at T=10');
+     
+     
+     subplot(2,2,3);     
+     imshow(Lbin2);
+     title('Image Laplacian after thresholding at T=15');
+     
+     
+     subplot(2,2,4);     
+     imshow(Lbin3);
+     title('Image Laplacian after thresholding at T=20');
 end
