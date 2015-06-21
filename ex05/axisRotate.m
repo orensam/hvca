a50a5367c6875422db2a1e4c9ac46320be8a1f2c
Oren Samuel <oren.samuel@mail.huji.ac.il>
2015-06-21 19:02:16 +0300

    ex5 axis rotate fix

--- ex05/axisRotate.m
+++ ex05/axisRotate.m
@@ -9,16 +9,34 @@ function [newLoc] = axisRotate(loc, axis, theta)
     
     rotMat = eye(3);
     if axis == 'x'
-        rotMat(2:3,2:3) = [ct, st; -st, cost];
+        rotMat(2:3,2:3) = [ct, -st; st, ct];
     elseif axis == 'y'
         rotMat(1,1) = ct;
-        rotMat(1,3) = -st;
-        rotMat(3,1) = st;
-        rotMat(3,3) = ct;
+        rotMat(1,3) = st;
+        rotMat(3,1) = -st;
+        rotMat(3,3) = ct;        
     else
-        rotMat(1:2,1:2) = [ct, st; -st, cost];
+        rotMat(1:2,1:2) = [ct, -st; st, ct];
     end
+    
+    
     
     newLoc = rotMat * loc;
     
+end
+
+function R=bla(x, y, z, teta)
+norm=sqrt(x.^2 + y.^2 + z.^2);
+x=x/norm;
+y=y/norm;
+z=z/norm;
+R(1,1) = (x.^2+(1-x.^2).*cos(teta));
+R(1,2) = (x.*y.*(1-cos(teta)) -z.*sin(teta));
+R(1,3) = (x.*z.*(1-cos(teta)) +y.*sin(teta));
+R(2,1) = (x.*y.*(1-cos(teta)) +z.*sin(teta));
+R(2,2) = (y.^2+(1-y.^2).*cos(teta));
+R(2,3) = (y.*z.*(1-cos(teta)) -x.*sin(teta));
+R(3,1) = (x.*z.*(1-cos(teta)) -y.*sin(teta));
+R(3,2) = (y.*z.*(1-cos(teta)) +x.*sin(teta));
+R(3,3) = (z.^2+(1-z.^2).*cos(teta));
 end
